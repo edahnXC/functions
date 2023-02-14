6490e945a1bd651700c14cf374fd1404f6ac22b1
edahnXC <edahnxc@gmail.com>
2023-02-14 05:47:33 +0530

    assignment-8

---
+++ oop_8_q4.py
@@ -0,0 +1,11 @@
+def set_name(self, name):
+    self.__name = name
+
+def get_name(self):
+    return self.__name
+
+def set_population(self, population):
+    self.__population = population
+
+def get_population(self):
+    return self.__population
